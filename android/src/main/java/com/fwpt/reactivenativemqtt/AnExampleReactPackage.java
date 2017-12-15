e8868aada2d6ce7bedb04a72a1c399351f1ad046
xuym <414191516@qq.com>
2017-12-15 09:49:19 +0800

    提交兼容新版本package代码

--- android/src/main/java/com/fwpt/reactivenativemqtt/AnExampleReactPackage.java
+++ android/src/main/java/com/fwpt/reactivenativemqtt/AnExampleReactPackage.java
@@ -18,12 +18,12 @@ import java.util.List;
 public class AnExampleReactPackage implements ReactPackage {
 
 
-    @Override
+
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
+
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
